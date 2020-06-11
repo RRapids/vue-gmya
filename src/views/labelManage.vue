552015e9cc207dce2fa1a70bdcafa2220003762a
rapids <tengfeiqaq@gmail.com>
2020-06-11 17:08:53 +0800

    留言管理我的

--- src/views/labelManage.vue
+++ src/views/labelManage.vue
@@ -12,8 +12,8 @@
       </div>
       <!-- 有评论时 -->
       <div v-else class="detail">
-        <img :src="item.useravatar" class=" icon usericon" />
-        <img src="../asset/delete.png" class="icon deleteicon" />
+        <img :src="item.useravatar" class=" icon" />
+        <img src="../asset/delete.png" class="icon" />
         <input
           id="label-input"
           class="label-input"
@@ -23,6 +23,18 @@
         />
       </div>
     </div>
+    <!-- 人员详情 -->
+    <div class="userdetail" v-show="true">
+      <p class="p2">评论人</p>
+      <div class="usercontent">
+        <img src="../asset/bg_weidenglu.png" class="usericon" />
+        <div class="name-time">
+          <p>腾飞</p>
+          <p>2020.06.06 11:14</p>
+        </div>
+      </div>
+    </div>
+    <img src="../asset/kefu.png" class="kefu" />
   </div>
 </template>
 
@@ -121,5 +133,48 @@ export default {
   width: 200px;
   outline: none;
   vertical-align: middle; //表单控件上下居中对齐,解决输入文字时，下边框微微移动问题
+}
+.kefu {
+  width: 50px;
+  height: 50px;
+  margin-top: 50%;
+  margin-left: 70%;
+  z-index: 1;
+  border-radius: 50%;
+}
+.userdetail {
+  width: 70%;
+  height: 150px;
+  z-index: 99;
+  margin: 0 auto;
+  margin-top: 20%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19);
+  border: 1px solid #eeeeee;
+}
+.usercontent {
+  display: flex;
+  height: 40px;
+  background-color: #eeeeee;
+  text-align: left;
+  align-items: center;
+  margin: 1px 10px 1px 10px;
+}
+.usericon {
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+}
+.name-time {
+  display: flex;
+  height: 30px;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 10px;
+}
+.p2 {
+  text-align: left;
+  font-size: 12px;
+  margin-left: 10px;
 }
 </style>
